7f8913219c0a94b15064285b79b8947aabd68dde
Your Name <you@example.com>
2025-12-17 15:49:17 +0100

    'reer'

--- standalone.py
+++ standalone.py
@@ -2,28 +2,32 @@ import os
 from threading import Thread
 from time import sleep
 import socket
+from waitress import serve
+from myproject.wsgi import application  # Replace with your project name
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("8.8.8.8", 80))
+        s.connect(("8.8.8.8", 80))  # Just a way to get local IP
         ip = s.getsockname()[0]
     finally:
         s.close()
     return ip
 
 ip = get_local_ip()
-def runserver():
-    os.system(f'python manage.py runserver {ip}:80')
 
-def lunchchrome():
-    sleep(2)  # wait for server
+def runserver():
+    # Use Waitress to serve Django app
+    serve(application, host=ip, port=80)
+
+def launchchrome():
+    sleep(2)  # Give Waitress time to start
     os.system(f'start chrome http://{ip}')
 
+# Run the server and launch Chrome in parallel
 t1 = Thread(target=runserver)
-t2 = Thread(target=lunchchrome)
+t2 = Thread(target=launchchrome)
 
 t1.start()
-sleep(2)
+sleep(2)  # Give some time for Waitress to initialize
 t2.start()
-
