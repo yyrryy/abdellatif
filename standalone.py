e67488303b86dd81195435c0a2d704a1d9e795a6
Your Name <you@example.com>
2025-12-17 14:15:37 +0100

    'reer'

--- standalone.py
+++ standalone.py
@@ -6,26 +6,24 @@ import socket
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("8.8.8.8", 80))  # Just a way to get local IP
+        s.connect(("8.8.8.8", 80))
         ip = s.getsockname()[0]
     finally:
         s.close()
     return ip
 
 ip = get_local_ip()
-
 def runserver():
-    # Use Gunicorn to run Django instead of runserver
-    os.system(f'gunicorn myproject.wsgi:application --bind {ip}:80')
+    os.system(f'python manage.py runserver {ip}:80')
 
-def launchchrome():
-    sleep(2)  # Give Gunicorn time to start
-    os.system(f'start chrome http://{ip}:80')
+def lunchchrome():
+    sleep(2)  # wait for server
+    os.system(f'start chrome http://{ip}')
 
-# Run the server and launch Chrome in parallel
 t1 = Thread(target=runserver)
-t2 = Thread(target=launchchrome)
+t2 = Thread(target=lunchchrome)
 
 t1.start()
-sleep(2)  # Give some time for Gunicorn to initialize
+sleep(2)
 t2.start()
+
