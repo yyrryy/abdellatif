b4c99a64e840d975ac16b7e773d0a5a3bf16ef1f
Your Name <you@example.com>
2025-12-19 15:32:50 +0100

    'reer'

--- products/views.py
+++ products/views.py
@@ -4705,7 +4705,7 @@ def excelpdcts(request):
         name = d.name
         mark = None if pd.isna(d.mark) else d.mark
         order = None if pd.isna(d.order) else d.order
-        min = None if pd.isna(d.min) else d.min
+        minstock = None if pd.isna(d.minstock) else d.minstock
         refeq = '' if pd.isna(d.refeq) else d.refeq
         diam = '' if pd.isna(d.diam) else d.diam
         qty = 0 if pd.isna(d.qty) else d.qty
@@ -4733,7 +4733,7 @@ def excelpdcts(request):
         uniqcode=f'pdct{lastid+1}'
         product=Produit.objects.create(
             uniqcode=uniqcode,
-            minstock=min,
+            minstock=minstock,
             ref=ref,
             equivalent=refeq,
             refeq1=refeq,
