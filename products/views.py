9d180caba41b071e949e5e21c02c6e557fd548c2
Your Name <you@example.com>
2025-12-21 12:04:42 +0100

    'reer'

--- products/views.py
+++ products/views.py
@@ -586,7 +586,8 @@ def updateproduct(request):
             'category_id':request.POST.get('category'),
             'mark_id':request.POST.get('marque'),
             'diametre':request.POST.get('diametre'),
-            'stock':product.stocktotal
+            'stock':product.stocktotal,
+            'uniqcode':product.uniqcode
         }
         # if image:
         #     data['image']=product.image.url.replace('/media/', '') if product.image else '/media/default.png',
