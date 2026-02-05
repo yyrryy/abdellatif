d109a717cf1cf4e55272f71e42d1687ddfd9e427
yurey <aitaliabdelwahed@gmail.com>
2026-02-05 18:22:38 +0100

    reer

--- standalone.py
+++ standalone.py
@@ -2,35 +2,53 @@ import os
 from threading import Thread
 from time import sleep
 import socket
+import subprocess
 from waitress import serve
 from autoparts.wsgi import application  # Replace with your project name
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(("8.8.8.8", 80))  # Just a way to get local IP
+        s.connect(("8.8.8.8", 80))
         ip = s.getsockname()[0]
     except:
-        s.close()
-        ip='localhost'
+        ip = 'localhost'
     finally:
         s.close()
     return ip
 
 ip = get_local_ip()
+local_port = 80       # Local port for Waitress
+remote_port = 8081      # Port on remote server
+remote_user = "boxer"
+remote_host = "167.71.77.64"
 
 def runserver():
-    # Use Waitress to serve Django app
-    serve(application, host=ip, port=80)
+    # Serve your Django app with Waitress
+    serve(application, host=ip, port=local_port)
 
-def launchchrome():
-    sleep(2)  # Give Waitress time to start
-    os.system(f'start chrome http://{ip}')
+def start_ssh_tunnel():
+    sleep(2)  # Give Waitress a moment to start
+    # Run SSH reverse tunnel in background
+    cmd = [
+        "ssh",
+        "-fN",  # Run in background, no remote command
+        "-R", f"{remote_port}:{ip}:{local_port}",
+        f"{remote_user}@{remote_host}"
+    ]
+    print("Starting SSH reverse tunnel...")
+    subprocess.Popen(cmd)
+    print(f"Tunnel established: {remote_host}:{remote_port} -> {ip}:{local_port}")
 
-# Run the server and launch Chrome in parallel
-t1 = Thread(target=runserver)
-t2 = Thread(target=launchchrome)
+def launch_chrome():
+    sleep(4)  # Give Waitress a bit more time
+    os.system(f'start chrome http://{ip}:{local_port}')
 
-t1.start()
-sleep(2)  # Give some time for Waitress to initialize
-t2.start()
+# Start server, SSH tunnel, and optional Chrome in parallel
+t_server = Thread(target=runserver)
+t_ssh = Thread(target=start_ssh_tunnel)
+t_chrome = Thread(target=launch_chrome)
+
+t_server.start()
+t_ssh.start()
+t_chrome.start()
